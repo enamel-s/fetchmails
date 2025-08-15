119d319887be6f35b4651a6bda505ddc05b1cc8a
enamel-s <kaungk909k@gmail.com>
2025-08-15 23:05:34 +0630

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -3,9 +3,9 @@ import pandas as pd
 from io import StringIO
 
 url = "https://docs.google.com/spreadsheets/d/10O4IwokK1rNG3AAsWRiGzGq8Ge1gpt5sNsRuRKOSGcQ/export?format=csv&gid=0"
-
 response = requests.get(url)
 response.raise_for_status()
 
 df = pd.read_csv(StringIO(response.text))
-print(df.to_string(index=False))
+df.to_csv("sheet_data.csv", index=False)
+print("CSV updated successfully!")
